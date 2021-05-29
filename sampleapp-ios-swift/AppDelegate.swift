b463ba1c402b73ef5380634be9faaeb689f65736
babizoh <35225411+babizoh@users.noreply.github.com>
2021-05-29 13:38:59 +0900

    Update AppDelegate.swift
    add app secret

--- sampleapp-ios-swift/AppDelegate.swift
+++ sampleapp-ios-swift/AppDelegate.swift
@@ -10,6 +10,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        AppCenter.start(withAppSecret: "0d5c9582-3c12-42c9-bd74-b6c766fa304e", services:[
+            Analytics.self,
+            Crashes.self
+        ])
+
+        return true
+    }
+    
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
